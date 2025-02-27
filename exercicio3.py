c4781347006dbf849d0740b28e41b6dafb843fb5
Rodrigo Miranda <rodrigomiranda.rj@gmail.com>
2025-02-26 21:32:53 -0300

    Adicionado Exercicio3.py

---
+++ exercicio3.py
@@ -0,0 +1,8 @@
+import re
+
+def check_character(string):
+    rule = re.compile(r'[^a-zA-Z0-9]')
+    string = rule.search(string)
+    return not bool(string)
+
+print(check_character("ADFBSDSDGGjjfdjdj233545"))
